041a04e708d2cac52111fb3a49f012be694382ff
zzycami@gmail.com <zzycami@gmail.com@5949f6d6-2ae0-4945-84d0-7aebe08da0ec>
2012-04-11 13:14:00 +0000

    增加了游戏音乐

---
+++ trunk/ suduku/Sudoku/src/cn/edu/hdu/Music.java
@@ -0,0 +1,25 @@
+package cn.edu.hdu;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class Music {
+	private static MediaPlayer mediaPlayer = null;
+	
+	// stop the old music and start new one
+	public static void play(Context context, int resource){
+		stop(context);
+		mediaPlayer = MediaPlayer.create(context, resource);
+		mediaPlayer.setLooping(true);
+		mediaPlayer.start();
+	}
+	
+	// stop music
+	public static void stop(Context context){
+		if(mediaPlayer != null){
+			mediaPlayer.stop();
+			mediaPlayer.release();
+			mediaPlayer = null;
+		}
+	}
+}
